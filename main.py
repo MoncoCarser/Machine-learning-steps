3a7d190449dc3327910e3fb30767b8891ef44da3
Pasi P <pjpakkala@gmail.com>
2023-01-31 20:06:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#1 import data
+#2 clean the data
+#3 split the data -> training and test set
+    #80/20 about, good divide
+#4 create a model 
+#5 check the output
+#6 improve (possible steps 3 and/or 4)
